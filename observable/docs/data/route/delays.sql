a95f8128fde31470f8fc887176a7207e3f6d1154
JanneCools <janne.cools.5@hotmail.com>
2024-05-10 14:34:31 +0200

    feat: add texts

--- observable/docs/data/route/delays.sql
+++ observable/docs/data/route/delays.sql
@@ -1,4 +1,5 @@
-select t.route_id as route, t.id as train, s2.commercial_name as station, round(avg(s.arrival_delay)) as arrival_delay, round(avg(s.departure_delay)) as departure_delay
+select t.route_id as route, t.id as train, s2.commercial_name as station,
+       avg(s.arrival_delay) / 60 as arrival_delay, avg(s.departure_delay) / 60 as departure_delay
 from trains t inner join public.stops s on t.id = s.train_id inner join public.stations s2 on s2.id = s.station_id
 group by t.route_id, t.id, s2.commercial_name
 order by t.route_id;
