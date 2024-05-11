d71778d875f56f41af5cfad959dcb481327941ce
Jens Pots <jens@jenspots.com>
2024-05-11 17:03:06 +0200

    fix: filter stations on stop count

--- observable/docs/data/station/stations.sql
+++ observable/docs/data/station/stations.sql
@@ -1,6 +1,13 @@
 SELECT
-    id,
+    stations.id as id,
     commercial_name as name,
     latitude,
-    longitude
+    longitude,
+    count(*) as count
 FROM stations
+
+LEFT JOIN public.stops s on stations.id = s.station_id
+
+GROUP BY stations.id, commercial_name, latitude, longitude
+
+HAVING count(*) > 1000
